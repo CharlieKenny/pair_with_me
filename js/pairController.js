3c530f23176836a54253ef369513d00c01550be7
Tim Oxman <tim.oxman@gmail.com>
2015-06-25 15:49:58 +0100

    stable-ish version

--- js/pairController.js
+++ js/pairController.js
@@ -3,6 +3,8 @@ pairWithMe.controller('PairWithMeCtrl', function() {
 
   var yourself = 0;
 
+  self.choice = '';
+
 // maybe better to split the relationships into a seperate data store, rather than include them within each maker within cohort
   self.cohort = [
     {
@@ -46,7 +48,15 @@ pairWithMe.controller('PairWithMeCtrl', function() {
      paired: false
     }
   ];
-  self.relations =[];
+
+  self.relations =[
+                    // {"pair1": 0, "pair2": 1}
+                    // {"pair1": 3, "pair2": 4}
+                    // {"pair1": 7, "pair2": 0}
+                  ];
+  // self.blacklist = [
+  //                   {"blacklistOwner: 1, "members"{[0,2,3,4]} }
+  //                   ];
 
   this.pairWith = function(pair){
     if (yourself !== 0){
@@ -54,12 +64,26 @@ pairWithMe.controller('PairWithMeCtrl', function() {
         self.cohort[yourself-1].paired = true;
         var relationship = {"pair1": yourself, "pair2": pair};
         self.relations.push(relationship);
+        self.choice = self.cohort[yourself-1].name + ' has been paired with ' + self.cohort[pair-1].name;
     }
   };
 
-  this.setYourself = function(usernumber){
-        yourself = usernumber;
+  this.setYourself = function(userNumber){
+        yourself = userNumber;
   };
+
+  this.pairedWithMe = function(userNumber){
+        var noOfPairs = 0;
+        self.relations.forEach(function(relationship){
+            // if ( relationship.pair1 === yourself && relationship.pair2 === userNumber )
+            //     { noOfPairs++ };
+            // if ( relationship.pair2 === yourself && relationship.pair1 === userNumber )
+            //     { noOfPairs++ };
+            // return noOfPairs;
+        });
+            return 0;
+  };
+
 
   this.shyPairWith = function(pair){
         console.log("You are shy paired");
@@ -68,11 +92,16 @@ pairWithMe.controller('PairWithMeCtrl', function() {
   };
 
   this.isButtonDisplayed = function(usernumber){
-    // don't display the button, if you are the person selected, or you already have a pair.
+    // don't display the button, if you are the person selected, or you the maker already have a pair or yourself already has a pair.
+    if ( yourself == 0 ) return false;  // can't pair if you haven't selected yourself
     if ( yourself === usernumber ) return false
     else if ( self.cohort[usernumber-1].paired === true ) return false
+    else if ( self.cohort[yourself-1].paired === true ) return false
     else return true;
   };
 
+  this.displayFinalPairs = function(usernumber){
+        alert("Ashley has paired with Alex again, \nTim has paired with Andy, \nStefan is with Bristol ");
+  };
 
 });
--- test/e2e/pairWithMe_feature_spec.js
+++ test/e2e/pairWithMe_feature_spec.js
@@ -2,10 +2,11 @@ describe('Pair With Me', function() {
 
   var listItems = element.all(by.repeater('maker in pairCtrl.cohort'));
   var firstThisIsMeButton = listItems.get(0).element(by.id('thisIsMe'));
+  var danThisIsMeButton = listItems.get(3).element(by.id('thisIsMe'));
   var danPairButton = listItems.get(3).element(by.id('pairButton'));
 
   beforeEach(function(){
-    browser.get('http://localhost:8080');
+    browser.get('http://localhost:3000');
   });
 
 
@@ -22,15 +23,49 @@ describe('Pair With Me', function() {
   });
 
   it('a button is displayed and disappears if you select the individual', function() {
-    expect(listItems.get(0).getText()).toContain('Pair with this person');
+    expect(listItems.get(0).getText()).not.toContain('Pair with this person');
     firstThisIsMeButton.click();
+    expect(listItems.get(3).getText()).toContain('Pair with this person');
+    danPairButton.click();
     expect(listItems.get(0).getText()).not.toContain('Pair with this person');
   });
 
   it('once pairButton pressed, all pair buttons disappear', function() {
+    var allPairButtons = element(by.css('#pairButton'));
     firstThisIsMeButton.click();
     danPairButton.click();
-    expect(listItems.getText()).not.toContain('Pair with this person');
+    expect(allPairButtons.isPresent()).toBeFalsy();
   });
+
+  it('once pairButton pressed, both pairs displayed as unavailable', function() {
+    firstThisIsMeButton.click();
+    expect(listItems.get(0).getText()).toContain('false');
+    expect(listItems.get(1).getText()).toContain('false');
+    expect(listItems.get(3).getText()).toContain('false');
+    danPairButton.click();
+    expect(listItems.get(0).getText()).toContain('true');
+    expect(listItems.get(1).getText()).toContain('false');
+    expect(listItems.get(3).getText()).toContain('true');
+  });
+
+  it('once PairButton pressed, message displays to confirm selection', function() {
+    firstThisIsMeButton.click();
+    danPairButton.click();
+    expect(element(by.id('choiceDisplay')).getText()).toContain('Alex has been paired with Dan B');
+  });
+
+  it('once PairButton pressed, the screen shows who you are paired with', function() {
+    firstThisIsMeButton.click();
+    danPairButton.click();
+    expect(listItems.get(3).getText()).toContain('true true');
+    danThisIsMeButton.click();
+    expect(listItems.get(0).getText()).toContain('true true');
+    expect(listItems.get(1).getText()).toContain('false false');
+  });
+
+
+
+
+
 
 });
